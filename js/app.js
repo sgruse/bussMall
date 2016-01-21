a48a510664011bc5878a8c390ba38d2e15245886
Sam Gruse <sgruse89@gmail.com>
2016-01-21 09:57:30 -0800

    Linting branch on Thursday morning with updated CSS and D.R.Y

--- js/app.js
+++ js/app.js
@@ -1,56 +1,51 @@
-'use strict'
+'use strict';
 
 var allNames = ['bag', 'banana', 'boots', 'chair', 'chtulhu', 'dragon', 'pen', 'scissors', 'shark', 'sweep', 'unicorn', 'usb', 'water_can', 'wine_glass'];
 var allSources =['img/bag.jpg', 'img/banana.jpg', 'img/boots.jpg', 'img/chair.jpg', 'img/cthulhu.jpg', 'img/dragon.jpg', 'img/pen.jpg', 'img/scissors.jpg', 'img/shark.jpg', 'img/sweep.png', 'img/unicorn.jpg', 'img/usb.gif', 'img/water-can.jpg', 'img/wine-glass.jpg'];
 var locationsArray = [];
-function Images(name, fileLoc) {         //Constructor funciton to hold properties for all images.
+function Images(name, fileLoc) {                 //Constructor funciton to hold properties for all images.
   this.name = name;
   this.fileLoc = fileLoc;
-  this.timesClicked = 0;                          //timesClicked = total times clicked
+  this.timesClicked = 0;                         //timesClicked = total times clicked
   this.timesDisplayed = 0;
   this.percentClicked = 0;
-    this.percentClicked = 0;    //Adding in counters
-  }                                                 //Create method to calculate the % displayed
-  var totalClicks = 0;                             //Global total clicks counting off of handler
-
-function randomNumber(){
-  return Math.floor((Math.random() * locationsArray.length));        //randomNumber() returns random number
-}
+  this.percentClicked = 0;                       //Adding in counters
+}                                                //Create method to calculate the % displayed
+var totalClicks = 0;                             //Global total clicks counting off of handler
 
 for (var i = 0; i < allNames.length; i++) {
   locationsArray.push(new Images(allNames[i], allSources[i]));
-};
+}
+
+function randomNumber(){
+  return Math.floor((Math.random() * locationsArray.length));
+}
 
 var img1 = document.getElementById('first');
 var img2 = document.getElementById('second');
 var img3 = document.getElementById('third');
-var rand1;
-var rand2;
-var rand3;
+var rand1, rand2, rand3;
 
 function random(){
-  //img1 = document.getElementById('first');
   rand1 = randomNumber();
   img1.src = locationsArray[rand1].fileLoc;
   locationsArray[rand1].timesDisplayed++;               //This is equallying times displayed
 
-  //img2 = document.getElementById('second');
   rand2 = randomNumber();
   while (rand1 === rand2) {
-  rand2 = randomNumber();
-}
+    rand2 = randomNumber();
+  }
   img2.src = locationsArray[rand2].fileLoc;
   locationsArray[rand2].timesDisplayed++;
 
-  //img3 = document.getElementById('third')
   rand3 = randomNumber();
   while (rand1 === rand3 || rand2 === rand3) {
   rand3 = randomNumber();
-}
+  }
   img3.src = locationsArray[rand3].fileLoc;
   locationsArray[rand3].timesDisplayed++;
 }
-  random();
+random();
 
 function handleChangeImage(image) {
   image.timesClicked++;
@@ -58,6 +53,16 @@ function handleChangeImage(image) {
   checkButton();
   random();
   image.percentClicked = (parseInt(image.timesClicked) / parseInt(image.timesDisplayed)).toFixed(2) * 100;
+}
+
+function checkButton() {
+  var hidden;
+  if (totalClicks < 1){
+    // locationsArray.length
+    resultButton.removeAttribute(hidden);
+  } else {
+    resultButton.style.display = "block"
+  }
 }
 
 first.addEventListener('click', function(){
@@ -75,33 +80,26 @@ third.addEventListener('click', function(){
 resultButton.addEventListener('click', makeChartOne);
 resultButton.addEventListener('click', makeChartTwo);
 
-  function checkButton() {
-    var hidden;
-    if (totalClicks < 1){
-      // locationsArray.length
-      resultButton.removeAttribute(hidden);
-    } else {
-      resultButton.style.display = "block"
-    }
-  }
-
 function makeChartOne() {
   document.getElementById('chartOneHeader').innerHTML = '';
   var allClicks = [];
   var allDisplays = [];
-  var header = document.createElement('h3');
+  var header = document.createElement('chartOneHeader');
   header.textContent = 'Times displayed vs. Times chosen';
+  var headerTwo = document.createElement('chartTwoHeader');
+  headerTwo.textContent = 'Percentage Displayed';
   chartOneHeader.appendChild(header);
+  chartTwoHeader.appendChild(headerTwo);
   for (var i = 0; i < locationsArray.length; i++) {
-  allClicks[i] = locationsArray[i].timesClicked;
-  allDisplays[i] = locationsArray[i].timesDisplayed;
-}
+    allClicks[i] = locationsArray[i].timesClicked;
+    allDisplays[i] = locationsArray[i].timesDisplayed;
+  }
 
   var data = {
-      labels: ["bag", "banana", "boots", "chair", "cthulhu", "dragon", "pen", "scissors", "shark", "sweep", "unicorn", "usb", "water_can", "wine_glass"],
-      datasets: [
-          {
-              label: "My First dataset",
+    labels: ["bag", "banana", "boots", "chair", "cthulhu", "dragon", "pen", "scissors", "shark", "sweep", "unicorn", "usb", "water_can", "wine_glass"],
+    datasets: [
+      {
+      label: "My First dataset",
               fillColor: "rgb(225,234,253)",
               // strokeColor: "rgba(62,101,178,0.7)",
               highlightFill: "rgb(94,131,205)",
